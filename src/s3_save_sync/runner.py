c56c040144d4c8466a627379732b16461a1c668f
Jonathan Pecar <jonathan.pecar@outlook.com>
2025-02-09 16:57:51 +1100

    Added an intermittent synchronising loop

--- src/s3_save_sync/runner.py
+++ src/s3_save_sync/runner.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from watchdog.observers import Observer
+from time import sleep
 
 from .logging import LOGGER
 from .games.game import load_games_from_toml, LocalGame
@@ -8,7 +9,7 @@ from . import APP_PATH
 from .games.bucket import BucketGame
 from .monitor import SaveSyncher
 
-def run(initial_sync: bool, monitor: bool):
+def run(initial_sync: bool, monitor: bool, sync_freq: int = -1):
     default_games = load_games_from_toml(Path(__file__).parent / 'games' / "default_games.toml")
     custom_games = load_games_from_toml(APP_PATH / "game_config.toml")
     games = [g for g in custom_games]
@@ -19,6 +20,9 @@ def run(initial_sync: bool, monitor: bool):
 
     if monitor:
         observer = Observer()
+    
+    if sync_freq > 0:
+        synchers = []
 
     for game in games:
         if not game.saves_exist():
@@ -26,10 +30,13 @@ def run(initial_sync: bool, monitor: bool):
             continue
 
         bucket_game = BucketGame(game.key)
+        syncher = SaveSyncher(bucket_game, game)
         if initial_sync:
-            SaveSyncher(bucket_game, game).synchronise()
+            syncher.synchronise()
         if monitor:
-            observer.schedule(SaveSyncher(bucket_game, game), path=game.path, recursive=True)
+            observer.schedule(syncher, path=game.path, recursive=True)
+        if sync_freq > 0:
+            synchers.append(syncher)
     
     if monitor:
         observer.start()
@@ -40,7 +47,12 @@ def run(initial_sync: bool, monitor: bool):
             observer.stop()
             observer.join()
 
-
+    if sync_freq > 0:
+        LOGGER.info(f"Starting synchronisation loop at {sync_freq} seconds")
+        while True:
+            sleep(sync_freq)
+            for syncher in synchers:
+                syncher.synchronise()
 
 
         
